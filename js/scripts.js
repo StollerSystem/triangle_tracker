70cb37ffb4e77b1a0fcfbe1363d1b74e5adfe8a2
Ben Stoller <stollersystemsound@gmail.com>
2020-08-13 16:15:19 -0700

    finish triangle logic
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,31 @@
 
 function calcTri (side1, side2, side3) {
   let result;    
-  if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <=side3) {
-    let result = "Not a Triangle"      
+  if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <=side1) {
+    result = "not a Triangle"      
   }
     else if (side1 === side2 && side2 === side3) {
-      let result = "Equilateral";        
+      result = "equilateral";        
   } else if (side1 === side2 || side2 === side3 || side1 == side3) {
-      let result = "Isosceles";      
+      result = "isosceles";      
   } else {
-      let result = "Scalene";
+      result = "scalene";
   }
+  console.log(result);
   return result;
 }
 
 // Interface Logic 
 
-$(document).DOMMatrixReadOnly(function() {
-  $("#forOne").submit(function(event) {
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
     const side1 = parseInt($("#side1").val());
     const side2 = parseInt($("#side2").val());
     const side3 = parseInt($("#side3").val());
     
-    const result = calcTri(side1, side2, side3)
-    $("#result").text("This is a/an " + " triangle.");
+    let result = calcTri(side1, side2, side3);
+    $("#result").text("This is a/an " + result + " triangle.");
   });
 });
 
